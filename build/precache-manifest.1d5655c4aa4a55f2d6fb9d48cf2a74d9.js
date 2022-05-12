32a18e4e4370a149f783cefb4d78155e1dec7bf7
Rashmi Deshpande <rashmird32@gmail.com>
2022-05-12 16:08:04 +0530

    Add files via upload

---
+++ build/precache-manifest.1d5655c4aa4a55f2d6fb9d48cf2a74d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccdcfeaa8c4038a0a30c6262b029789f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16ecfb728dd87440e6c0",
+    "url": "/static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "047f6c63a34c086323ae",
+    "url": "/static/js/2.2d7c86c6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.2d7c86c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16ecfb728dd87440e6c0",
+    "url": "/static/js/main.13f6c9f5.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
